143b070e4211e2399cbc79355f1f7595fda83d9d
Alan Favre <alan.fvr@eduge.ch>
2015-09-02 15:51:16 +0200

    Inscription dans la base de données fonctionnelle. Connexion mysql est bizarre : "mysql:'. HOST .'=;dbname='. DBNAME .'', USER, PASS" ?

--- dbfunction.php
+++ dbfunction.php
@@ -20,8 +20,31 @@ function getConnexion()
     return $bdd;
 }
 
-function ajouterUser($nom, $prenom, $email, $dateNaissance, $pseudo, $password, $passwordconfirm, $description)
+if(isset($_POST['envoyer']))
 {
+    ajouterUser();
+}
+
+function ajouterUser()
+{
+    $nom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_SPECIAL_CHARS);
+    $prenom = filter_input(INPUT_POST, 'prenom', FILTER_SANITIZE_SPECIAL_CHARS);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_SPECIAL_CHARS);
+    $dateNaissance = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_SPECIAL_CHARS);
+    $pseudo = filter_input(INPUT_POST, 'pseudo', FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, 'pass', FILTER_SANITIZE_SPECIAL_CHARS);
+    $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_SPECIAL_CHARS);
+    
+    
+    $data = getConnexion()->prepare('INSERT INTO user VALUES("", :nom, :prenom, :email, :date, :pseudo, :pass, :description)');
+    $data->bindParam(':nom', $nom, PDO::PARAM_STR);
+    $data->bindParam(':prenom', $prenom, PDO::PARAM_STR);
+    $data->bindParam(':email', $email, PDO::PARAM_STR);
+    $data->bindParam(':date', $dateNaissance, PDO::PARAM_STR);
+    $data->bindParam(':pseudo', $pseudo, PDO::PARAM_STR);
+    $data->bindParam(':pass', $password, PDO::PARAM_STR);
+    $data->bindParam(':description', $description, PDO::PARAM_STR);
+    $data->execute();
     
 }
 
--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <?php
-include 'dbfunction.php';
-getConnexion();
+
 ?>
 <html>
     <head>
@@ -14,15 +13,14 @@ getConnexion();
         <h1>Formulaire d'inscription</h1>
         <div>
         <form id="monformulaire" method="post" action="dbfunction.php">
-            <label for="nom" >Nom : </label><input id="nom" type="text" name="nom" /><br /><br />
-            <label for="prenom" >Prénom : </label><input id="prenom" type="text" name="prenom" /><br /><br />
-            <label for="email" >Email : </label><input id="email" type="email" name="email" /><br /><br />
-            <label for="date" >Date de naissance : </label><input id="date" type="date" name="date" /><br /><br />
-            <label for="pseudo" >Pseudo : </label><input id="pseudo" type="text" name="pseudo" /><br /><br />
-            <label for="pass" >Mot de passe : </label><input id="pass" type="password" name="pass" /><br /><br />
-            <label for="passconfirm" >Confirmation : </label><input id="passconfirm" type="password" name="passconfirm" /><br /><br />
-            <label for="description" >Description de vous : </label><textarea id="description" name="description"></textarea><br /><br />
-            <input id="reset" name="reset" type='reset' value='Rénitialiser' /><input name="submit" id="submit" type="submit" value="Envoyer" />
+            <label for="nom" >Nom : </label><input id="nom" type="text" name="nom" required /><br /><br />
+            <label for="prenom" >Prénom : </label><input id="prenom" type="text" name="prenom" required /><br /><br />
+            <label for="email" >Email : </label><input id="email" type="email" name="email" required /><br /><br />
+            <label for="date" >Date de naissance : </label><input id="date" type="date" name="date" required /><br /><br />
+            <label for="pseudo" >Pseudo : </label><input id="pseudo" type="text" name="pseudo" required /><br /><br />
+            <label for="pass" >Mot de passe : </label><input id="pass" type="password" name="pass" required /><br /><br />
+            <label for="description" >Description de vous : </label><textarea id="description" name="description" ></textarea><br /><br />
+            <input id="reset" name="reset" type='reset' value='Rénitialiser' /><input name="envoyer" id="submit" type="submit" value="Envoyer" />
         </form>
     </div>
     </body>
