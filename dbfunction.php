ffb5b7bf09f1bae44e3f5ccdff30a15803e431bc
Alan Favre <alan.fvr@eduge.ch>
2015-09-09 13:25:18 +0200

    Correction de l'insertion. Modification des POST en REQUEST et cryptage du mot de passe en sha1.

--- dbfunction.php
+++ dbfunction.php
@@ -10,31 +10,35 @@ function getConnexion()
         try 
         {
             $bdd = new PDO('mysql:'. HOST .'=;dbname='. DBNAME .'', USER, PASS);
+            /* mysql:127.0.0.1=;dbname=m151adminaf,m151admin,m151admin */
         } 
         catch (PDOException $e) 
         {
             print "Erreur : " . $e->getMessage() . "<br/>";
             die();
         }
+        
+        
     }
     return $bdd;
 }
 
-if(isset($_POST['envoyer']))
+if(isset($_REQUEST['envoyer']))
 {
     ajouterUser();
 }
 
 function ajouterUser()
 {
-    $nom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_SPECIAL_CHARS);
-    $prenom = filter_input(INPUT_POST, 'prenom', FILTER_SANITIZE_SPECIAL_CHARS);
-    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_SPECIAL_CHARS);
-    $dateNaissance = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_SPECIAL_CHARS);
-    $pseudo = filter_input(INPUT_POST, 'pseudo', FILTER_SANITIZE_SPECIAL_CHARS);
-    $password = filter_input(INPUT_POST, 'pass', FILTER_SANITIZE_SPECIAL_CHARS);
-    $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_SPECIAL_CHARS);
+    $nom = filter_input(INPUT_REQUEST, 'nom', FILTER_SANITIZE_SPECIAL_CHARS);
+    $prenom = filter_input(INPUT_REQUEST, 'prenom', FILTER_SANITIZE_SPECIAL_CHARS);
+    $email = filter_input(INPUT_REQUEST, 'email', FILTER_SANITIZE_SPECIAL_CHARS);
+    $dateNaissance = filter_input(INPUT_REQUEST, 'date', FILTER_SANITIZE_SPECIAL_CHARS);
+    $pseudo = filter_input(INPUT_REQUEST, 'pseudo', FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_input(INPUT_REQUEST, 'pass', FILTER_SANITIZE_SPECIAL_CHARS);
+    $description = filter_input(INPUT_REQUEST, 'description', FILTER_SANITIZE_SPECIAL_CHARS);
     
+    $password = sha1($password);
     
     $data = getConnexion()->prepare('INSERT INTO user VALUES("", :nom, :prenom, :email, :date, :pseudo, :pass, :description)');
     $data->bindParam(':nom', $nom, PDO::PARAM_STR);
