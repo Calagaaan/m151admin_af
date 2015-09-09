dc16a328caf2dbedbb0d3f41c30335fc7ba1801e
Alan Favre <alan.fvr@eduge.ch>
2015-09-09 13:30:05 +0200

    Modification des REQUEST en POST.

--- dbfunction.php
+++ dbfunction.php
@@ -30,13 +30,13 @@ if(isset($_REQUEST['envoyer']))
 
 function ajouterUser()
 {
-    $nom = filter_input(INPUT_REQUEST, 'nom', FILTER_SANITIZE_SPECIAL_CHARS);
-    $prenom = filter_input(INPUT_REQUEST, 'prenom', FILTER_SANITIZE_SPECIAL_CHARS);
-    $email = filter_input(INPUT_REQUEST, 'email', FILTER_SANITIZE_SPECIAL_CHARS);
-    $dateNaissance = filter_input(INPUT_REQUEST, 'date', FILTER_SANITIZE_SPECIAL_CHARS);
-    $pseudo = filter_input(INPUT_REQUEST, 'pseudo', FILTER_SANITIZE_SPECIAL_CHARS);
-    $password = filter_input(INPUT_REQUEST, 'pass', FILTER_SANITIZE_SPECIAL_CHARS);
-    $description = filter_input(INPUT_REQUEST, 'description', FILTER_SANITIZE_SPECIAL_CHARS);
+    $nom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_SPECIAL_CHARS);
+    $prenom = filter_input(INPUT_POST, 'prenom', FILTER_SANITIZE_SPECIAL_CHARS);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_SPECIAL_CHARS);
+    $dateNaissance = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_SPECIAL_CHARS);
+    $pseudo = filter_input(INPUT_POST, 'pseudo', FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, 'pass', FILTER_SANITIZE_SPECIAL_CHARS);
+    $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_SPECIAL_CHARS);
     
     $password = sha1($password);
     
