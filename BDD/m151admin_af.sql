6d1a8d92483c19f4bf86b659b1061d3b2c75145e
Alan Favre <alan.fvr@eduge.ch>
2015-09-23 15:53:15 +0200

    Exportation de la base de données à jour.

--- BDD/m151admin_af.sql
+++ BDD/m151admin_af.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 23 Septembre 2015 à 12:55
+-- Généré le :  Mer 23 Septembre 2015 à 15:52
 -- Version du serveur :  5.6.15-log
 -- Version de PHP :  5.5.8
 
@@ -37,14 +37,18 @@ CREATE TABLE IF NOT EXISTS `user` (
   `description` text,
   PRIMARY KEY (`idUser`),
   UNIQUE KEY `pseudo` (`pseudo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=16 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
 
 --
 -- Contenu de la table `user`
 --
 
 INSERT INTO `user` (`idUser`, `nom`, `prenom`, `email`, `dateNaissance`, `pseudo`, `password`, `description`) VALUES
-(15, 'Favre', 'Alan', 'favre.alan@gmail.com', '1995-10-07', 'Calagaaan', 'a0b6bf60beef61e6fd2dc95363d267b65bb98e13', 'I like trains.');
+(15, 'Favreo', 'Alano', 'favre.alan@gmail.como', '1995-10-07', 'Calagaaano', 'a0b6bf60beef61e6fd2dc95363d267b65bb98e13', 'I like trainso.'),
+(16, 'Leroy', 'Sacha', 'sacha.leroy@gmail.com', '1995-12-20', 'eXtraspe6men', 'bb0446b9e918a5cf8a31b4caf9ba99122ddbd498', 'J&#39;aime bien Ruben.'),
+(17, 'CARVALHO', 'Ruben', 'theking@gmail.como', '1996-09-13', 'TheKiing', '2e4ee22a1ec3f9c182a37b90566a76a27735b98a', 'J&#39;aime plus bien Sacha.'),
+(18, 'RAEVEN', 'MARCELA', 'what@what.com', '1996-07-26', 'Raelo', '5ed25af7b1ed23fb00122e13d7f74c4d8262acd8', 'Salut c&#39;est MOI.'),
+(19, 'Bertrand', 'Nico', 'nicoBERTRAND@yolo.com', '1997-07-18', 'Orion24_', 'a518defe4470f463153dbaa8f10c4a5ccd5a4b31', 'J&#39;aimerai comprendre.');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
