97a68514ce2ec9f39c8d3c66cdd6ee0e1525325c
Alan Favre <alan.fvr@eduge.ch>
2015-09-30 15:27:05 +0200

    Mise en page de page de login.

--- login.php
+++ login.php
@@ -7,9 +7,9 @@
     </head>
     <body>
         <form method="post" action="dbfunctions.php">
-            <input type="text" name="pseudo" placeholder="Pseudo" />
-            <input type="password" name="pass" placeholder="Mot de Passe" />
-            <input type="submit" name="connexion" value="Se Connecter" />
+            <input type="text" name="pseudo" placeholder="Pseudo" /><br /><br />
+            <input type="password" name="pass" placeholder="Mot de Passe" /><br /><br />
+            <input type="submit" name="connexion" value="Se Connecter" /><br /><br />
         </form>
     </body>
 </html>
