76781762f845d0e2dcca2fc9b83306a2f7337a97
Alan Favre <alan.fvr@eduge.ch>
2015-11-04 12:55:18 +0100

    Mise à jour bdd.

--- BDD/m151admin_af.sql
+++ BDD/m151admin_af.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 30 Septembre 2015 à 14:38
+-- Généré le :  Mer 04 Novembre 2015 à 12:54
 -- Version du serveur :  5.6.15-log
 -- Version de PHP :  5.5.8
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `m151admin_af`
 --
+CREATE DATABASE IF NOT EXISTS `m151admin_af` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `m151admin_af`;
 
 -- --------------------------------------------------------
 
@@ -37,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `description` text,
   PRIMARY KEY (`idUser`),
   UNIQUE KEY `pseudo` (`pseudo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=24 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=25 ;
 
 --
 -- Contenu de la table `user`
@@ -46,8 +48,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`idUser`, `nom`, `prenom`, `email`, `dateNaissance`, `pseudo`, `password`, `description`) VALUES
 (20, 'Favre', 'Alan', 'favre.alan@gmail.com', '1995-10-07', 'Calagaaan', 'a0b6bf60beef61e6fd2dc95363d267b65bb98e13', 'Salut.'),
 (21, 'Muraro', 'Jeff', 'jeff.muraro@gmail.com', '1997-03-01', 'MrZukXx', 'f6889fc97e14b42dec11a8c183ea791c5465b658', 'Homme swag.'),
-(22, 'Leroy', 'Sacha', 'sachaleroy95@hotmail.fr', '1995-12-20', 'eXtraspe6men', '49b3d0d369b66b67148f4e610e06eed7362fe517', 'Je suppose...'),
-(23, 'Marcelo', 'Rae Vennedict', 'rae.mrcl@eduge.ch', '1996-07-26', 'baecone', '01b307acba4f54f55aafc33bb06bbbf6ca803e9a', 'Everyday i&#39;m stalkiling');
+(22, 'Leroy', 'Sacha', 'sachaleroy95@hotmail.fr', '1995-12-20', 'eXtraspe6men', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Je suppose...'),
+(24, 'Hello', 'CMoi', 'moi@moi.yolo', '1986-06-27', 'Declerq', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', 'Salut c&#39;est moi.');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
